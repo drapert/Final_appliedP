84f1f56ee57196ec4aca7a0f2e1cf695d12c3567
drapert <85189058+drapert@users.noreply.github.com>
2021-08-13 13:14:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,1 @@
-import random
-def Keno():
 
-    board = (range(1,80))
-    sim = random.sample(board,10)
-    keno = random.sample(board,10)
-
-    print('Computer picks: ',keno)
-    print ('Your picks: ', sim)
-    print('__________________________________________________________')
-
-    def intersection(sim, keno):
-       return(set(sim).intersection(keno))
-       
-    if len(intersection(sim,keno)) ==0:
-        return 0
-    else:
-        return 1
-Keno()
